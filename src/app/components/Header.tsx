b2b5fb85e8e5184675643c519691113d61917293
jdev <jlacandula@gmail.com>
2023-07-04 14:27:48 +0800

    header section

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="w-full h-12 opacity-20 md:fixed bg-white">
-      {/* Header content */}
+    <header className="w-full bg-primary md:absolute md:bg-transparent">
+      <div className="h-[47px] w-full bg-white opacity-20 md:h-[41px]"></div>
+      <h2 className="px-10 py-[22px] text-center text-2xl text-white md:px-[77px] md:text-left">
+        Brand New Company
+      </h2>
     </header>
   );
 };
